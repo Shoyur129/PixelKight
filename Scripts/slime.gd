f345f58245b263b38a07f3f963683edf9a9b2dbb
Shoyur129 <patrick.alaniz16@gmail.com>
2026-07-30 11:56:45 -0500

    Finalized level 1
    Level 1 fully completed with a scene transition and decorations

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
+
+const SPEED = 60.0
+
+var direction = 1
+
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1 #flip direction to left if colliding with right raycast
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
