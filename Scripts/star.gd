2e43014e8f47e34adaccc1014a171f677763851c
Shoyur129 <patrick.alaniz16@gmail.com>
2026-07-30 18:39:20 -0500

    added level_2
    added a level 2 scene

--- Scripts/star.gd
+++ Scripts/star.gd
@@ -2,5 +2,15 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Node2D and body.is_in_group("Player"):
+	var current_scene = get_tree().current_scene
+	if current_scene == null:
+		return
+
+	var is_player = body.is_in_group("player") or body.is_in_group("Player")
+	if not is_player:
+		return
+
+	if current_scene.scene_file_path == "res://Scenes/level_1.tscn":
+		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+	elif current_scene.scene_file_path == "res://Scenes/level_2.tscn":
 		get_tree().change_scene_to_file("res://Scenes/end.tscn")
